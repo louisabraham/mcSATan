d45250c843be2245deffe7cb802da67098a29601
louisabraham <louis.abraham@yahoo.fr>
2018-01-03 16:10:27 +0100

    change imports in logger

--- logger.py
+++ logger.py
@@ -1,8 +1,13 @@
 import logging
 from pprint import pformat
+from logging import INFO, DEBUG, WARNING
 
-logging.basicConfig(level=logging.WARNING)
+
+logging.basicConfig(level=WARNING)
 logger = logging.getLogger(__name__)
+
+isEnabledFor = logger.isEnabledFor
+
 debug = logger.debug
 info = logger.info
 warning = logger.warning
