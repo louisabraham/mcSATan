97f54a863ad6a6eb5f6439255411db4ef0b2d832
louisabraham <louis.abraham@yahoo.fr>
2018-07-27 09:31:17 +0200

    refactor cnf.py

--- mcSATan/cnf.py
+++ mcSATan/cnf.py
@@ -14,45 +14,51 @@ from mcSATan.core import Solver
 from mcSATan.logger import logger
 
 
+def parse(infile):
+    solver = Solver(CDCL=True)
+    buf = []
+    nClauses = float('inf')
+    for line in args.infile:
+        if not nClauses:
+            break
+        line = line.split()
+        if line[0] == 'c':
+            continue
+        if line[0] == 'p':
+            assert line[1] == 'cnf'
+            nVar = int(line[2])
+            nClauses = int(line[3])
+            variables = {i: solver.BoolVar('var%s' % i)
+                         for i in range(1, nVar + 1)}
+            continue
+        *line, = map(int, line)
+        line = buf + line
+        buf = []
+        if line[-1] != 0:
+            buf = line
+            continue
+        nClauses -= 1
+        solver.Clause(*[solver.Literal(variables[abs(i)], i > 0)
+                        for i in line[:-1]])
+    return solver
+
+
 parser = argparse.ArgumentParser()
 parser.add_argument('infile', nargs='?', type=argparse.FileType('r'),
                     default=stdin)
 parser.add_argument('--debug', nargs='?', type=int, default=30)
 
-args = parser.parse_args()
 
-dbt = time()
+if __name__ == '__main__':
 
-logger.setLevel(args.debug)
+    args = parser.parse_args()
+    logger.setLevel(args.debug)
 
-buf = []
-nClauses = float('inf')
-for line in args.infile:
-    if not nClauses:
-        break
-    line = line.split()
-    if line[0] == 'c':
-        continue
-    if line[0] == 'p':
-        assert line[1] == 'cnf'
-        solver = Solver(CDCL=True)
-        nVar = int(line[2])
-        nClauses = int(line[3])
-        variables = {i: solver.BoolVar('var%s' % i)
-                     for i in range(1, nVar + 1)}
-        continue
-    *line, = map(int, line)
-    line = buf + line
-    buf = []
-    if line[-1] != 0:
-        buf = line
-        continue
-    nClauses -= 1
-    solver.Clause(*[solver.Literal(variables[abs(i)], i > 0)
-                    for i in line[:-1]])
+    dbt = time()
+    solver = parse(args.infile)
+    print('Parsed %.02fs' % (time() - dbt), file=stderr)
 
-print('Parsed %.02fs' % (time() - dbt), file=stderr)
-dbt = time()
-solver.solve()
-pprint(solver.stats)
-print('Solved %.02fs' % (time() - dbt), file=stderr)
+    dbt = time()
+    solver.solve()
+    pprint(solver.stats)
+    print('Solved %.02fs' % (time() - dbt), file=stderr)
